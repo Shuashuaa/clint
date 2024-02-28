016aa78f31cfa877854a545a56f85bc0edf593b0
Shuashuaa <jovijoshuatania027@gmail.com>
2024-02-28 21:37:13 +0800

    022824

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueKinesis from "vue-kinesis";
 
 const app = createApp(App)
 
 app.use(createPinia())
+.use(VueKinesis)
 app.use(router)
 
 app.mount('#app')
