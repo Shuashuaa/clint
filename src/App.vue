6c4807a2f12dcb5b6e46c446f5110dbcbcaf16c1
Shuashuaa <jovijoshuatania027@gmail.com>
2024-11-26 22:49:37 +0800

    112624

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
-<script>
-export default {
-	setup() {
-		
-	},
-}
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+
 </script>
 
-
 <template>
+	<div class="bg-[#F7CAC9] dark:bg-[#242625]">
+		<Navbar class="sticky top-0 z-20"/>
+		<RouterView/>
+	</div>
 	
-	<main>
-		<router-view /> 
-	</main>
 </template>
+
+<style>
+body{
+	color: var(--font-white);
+	background-color: var(--bg-black);
+}
+</style>
