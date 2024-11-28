21666462e0ffce97757450f0b269d5de503ece9a
Shuashuaa <jovijoshuatania027@gmail.com>
2024-11-28 15:41:15 +0800

    112824

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ export default {
         DEFAULT: '0 2px 4px var(--tw-shadow-color)',
         lg: '0 8px 16px var(--tw-shadow-color)',
       },
+      fontFamily: {
+        "Alumni_Sans": ['Alumni Sans', 'sans-serif'],
+        "Alumni_Sans_Pinstripe": ['Alumni Sans Pinstripe', 'sans-serif'],
+        "Inter": ['Inter', 'sans-serif']
+      }
     },
   },
   plugins: [
