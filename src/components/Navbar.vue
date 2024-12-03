5ae594dbd8f16b4dcbabfc85fa0370069f824fae
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-03 10:30:03 +0800

    120324

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,20 +2,21 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted, watchEffect } from 'vue';
+import DarkMode from './DarkMode.vue'
 
 // burgerIcon
 
 const isMenuOpen = ref(false);
 
 const menuClass = computed(() => ({
-  'left-[-100%]': !isMenuOpen.value,
-  'left-[0px] bg-[#F7CAC9] dark:bg-[#242625]': isMenuOpen.value,
+	'left-[-100%]': !isMenuOpen.value,
+	'left-[0px] bg-[#F7CAC9] dark:bg-[#242625]': isMenuOpen.value,
 }));
 
 const burgerIcon = computed(() => (isMenuOpen.value ? 'pi pi-times' : 'pi pi-bars'));
 
 const toggleMenu = () => {
-  isMenuOpen.value = !isMenuOpen.value;
+  	isMenuOpen.value = !isMenuOpen.value;
 };
 
 // isScrolled
@@ -23,30 +24,18 @@ const toggleMenu = () => {
 const isScrollChanged = ref(false);
 
 const isScrolled = computed(() => ({
-  'transition-all bg-[#F7CAC9] dark:bg-[#242625]': !isScrollChanged.value,
-  'transition-all backdrop-filter backdrop-blur-lg bg-opacity-10': isScrollChanged.value,
+	'transition-all bg-[#F7CAC9] dark:bg-[#242625]': !isScrollChanged.value,
+	'transition-all backdrop-filter backdrop-blur-lg bg-opacity-10': isScrollChanged.value,
 }));
 
 onMounted(() => {
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 0 && !isScrollChanged.value) {
-      isScrollChanged.value = true;
-    } else if (window.scrollY <= 0 && isScrollChanged.value) {
-      isScrollChanged.value = false;
-    }
-  });
-});
-
-// isDarkMode
-
-const darkMode = ref(document.body.classList.contains('dark'));
-
-const toggleDarkMode = () => {
-    darkMode.value = !darkMode.value;
-};
-
-watchEffect(() => {
-    document.body.classList[darkMode.value ? 'add' : 'remove']('dark');
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 0 && !isScrollChanged.value) {
+			isScrollChanged.value = true;
+		} else if (window.scrollY <= 0 && isScrollChanged.value) {
+			isScrollChanged.value = false;
+		}
+	});
 });
 
 </script>
@@ -65,29 +54,27 @@ watchEffect(() => {
             </span>
         </div>
         
-        <ul class="top-[55px] md:flex md:items-center gap-5 z-[1] md:z-auto md:static absolute 
+        <ul class="top-[55px] md:flex md:items-center gap-10 z-[1] md:z-auto md:static absolute 
         w-full left-0 md:w-auto md:py-0 py-4 md:pl-0
         transition-all ease-in-out duration-500 text-white h-screen md:h-full
-        *:py-6 *:md:py-0" :class="menuClass">
+        *:py-6 *:md:py-0 *:text-[#242625] *:dark:text-white *:font-lato" :class="menuClass">
         
             <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="#" class="font-sans text-xl hover:text-[#244e48] duration-500">Home</a>
+                <a href="/" class="text-xl hover:text-[#244e48] duration-500">Home</a>
             </li>
             <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="#" class="font-sans text-xl hover:text-[#244e48] duration-500">Projects</a>
+                <a href="/projects" class="text-xl hover:text-[#244e48] duration-500">Projects</a>
             </li>
             <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="#" class="font-sans text-xl hover:text-[#244e48] duration-500">About</a>
+                <a href="#" class="text-xl hover:text-[#244e48] duration-500">About</a>
             </li>
             <li class="text-center">
-                <button class="font-sans bg-[#238374] text-white font-bold duration-500 px-6 py-2 hover:bg-[#9EC0BB] rounded">
+                <button class="bg-[#93a9d1] text-white font-bold duration-500 px-6 py-2 hover:bg-[#798499] rounded">
                     Hire me
                 </button>
             </li>
             <li class="text-center">
-                <label>
-                    <i @click="toggleDarkMode" :class="[darkMode ? 'pi pi-sun' : 'pi pi-moon']"></i>
-                </label>
+                <DarkMode/>
             </li>
         </ul>
     </nav>
