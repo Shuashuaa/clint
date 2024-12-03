289a0550c15d34c6e48d13a30605a83302c27d3a
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-03 10:30:16 +0800

    120324

---
+++ src/views/InfoView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+defineProps({
+    info: String
+})
+</script>
+
+<template>
+    <div class="flex justify-center w-full bg-white 
+    p-[15%] md:p-[5%] *:font-Inter *:text-lg ">
+    <!-- text-shadow shadow-[#bebebe] -->
+        <p class="md:w-[50svw]">
+            {{ info }}
+        </p>
+    </div>
+    
+</template>
