2b23b86c07bc53a2e8fdaa524def6013fb328b58
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-03 10:26:09 +0800

    120324

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -8,21 +8,21 @@ defineProps({
 </script>
 
 <template>
-    <div class="sm:h-[95svh]">
-        <div class="flex flex-col items-center pt-5 justify-center overflow-hidden sm:h-[95svh] bg-[#F7CAC9] bg-[radial-gradient(ellipse_80%_80%_at_80%_80%,rgba(179,206,229,1),rgba(255,255,255,0))] 
-        dark:bg-[#242625] dark:bg-[radial-gradient(ellipse_80%_80%_at_80%_80%,rgba(179,206,229,0.4),rgba(255,255,255,0))] transition-all ease-in-out md:relative">
+    <div>
+        <div class="flex flex-col items-center pt-5 justify-center overflow-hidden md:h-[95svh] bg-[#F7CAC9] bg-[radial-gradient(ellipse_80%_80%_at_80%_80%,#93a9d1,rgba(255,255,255,0))] 
+        dark:bg-[#242625] dark:bg-[radial-gradient(ellipse_80%_80%_at_80%_80%,#93a9d1,rgba(255,255,255,0))] transition-all ease-in-out md:relative">
             <p class="fontbgl text-[#ffd8d8] dark:text-[#2C2C2C] font-[sans-serif] font-black italic tracking-widest text-6xl md:text-9xl text-shadow-sm top-[20%] left-[10%] absolute">
                 {{ bgNameLeft }}
             </p>
-            <p class="fontbgr text-[#b6d1cb] dark:text-[#2C2C2C] font-[sans-serif] font-black italic tracking-widest text-6xl md:text-9xl text-shadow-sm top-[50%] right-[20%] md:top-[40%] md:right-[10%] absolute">
+            <p class="fontbgr text-[#b6d1cb] dark:text-[#2C2C2C] font-[sans-serif] font-black italic tracking-widest text-7xl md:text-8xl text-shadow-sm top-[50%] right-[20%] md:top-[40%] md:right-[10%] absolute">
                 {{ bgNameRight }}
             </p>
 
             <img draggable="false" class="w-[40svh] ml-[80px] md:w-[40svh] md:absolute md:top-0 z-10" src="../assets/imgs/top111524.png" alt="">
             <img draggable="false" class="w-[48svh] ml-[49px] mt-[-10px] md:w-[49svh] md:absolute md:top-[28%] md:rotate-[-5deg] z-10" src="../assets/imgs/center111524.png" alt="">
-            <img draggable="false" class="w-[47svh] ml-[-50px] mt-[-18px] md:w-[47svh] md:absolute md:bottom-[7%] z-10" src="../assets/imgs/bottom111524.png" alt="">
+            <img draggable="false" class="w-[47svh] ml-[-50px] mt-[-18px] md:w-[47svh] md:h-[50svh] md:absolute md:bottom-0 z-10" src="../assets/imgs/bottom111524.png" alt="">
             
-            <p class="text-white font-sans font-black italic tracking-widest text-lg md:text-3xl text-shadow-sm shadow-[black] bottom-[15%] md:bottom-[25%] absolute z-10">
+            <p class="text-white font-lato font-black italic tracking-widest text-lg md:text-3xl text-shadow-sm shadow-[black] bottom-[15%] md:bottom-[25%] absolute z-10">
                 {{ landingNameTitle }}
             </p>
         </div>
