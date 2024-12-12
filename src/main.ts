7d2292471dc4621b84d8d5c5381bd769cc1abdca
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-12 17:20:51 +0800

    121224

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// title meta
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title || '404';
+    next();
+});
+
 const app = createApp(App)
 
 app.use(router)
