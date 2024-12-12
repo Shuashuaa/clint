8718dea66d6b440995624d4128c3bb3ef755941e
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-12 16:04:33 +0800

    121224

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@ const isMenuOpen = ref(false);
 
 const menuClass = computed(() => ({
 	'left-[-100%]': !isMenuOpen.value,
-	'left-[0px] bg-[#F7CAC9] dark:bg-[#242625]': isMenuOpen.value,
+	'left-[0px]': isMenuOpen.value,
 }));
 
 const burgerIcon = computed(() => (isMenuOpen.value ? 'pi pi-times' : 'pi pi-bars'));
@@ -44,8 +44,9 @@ onMounted(() => {
     <!-- by default canvas is `display:block` so here if the canvas reach `md:` or more will flex the nav children components -->
     <nav class="md:flex md:items-center md:justify-between md:p-10" :class="isScrolled">
         <div class="px-5 py-2 md:p-0 flex justify-between items-center">
-            <span class="text-2xl text-white">
-                <img class="inline w-[50px] md:w-[100px]" src="../assets/imgs/clint.png" alt="">
+            <span class="text-2xl text-white relative">
+                <img class="absolute invisible dark:visible w-[50px] md:w-[100px]" src="../assets/imgs/CLINT-default.svg" alt="">
+                <img class="visible dark:invisible w-[50px] md:w-[100px]" src="../assets/imgs/CLINT-black.svg" alt="">
                 <!-- Clint. -->
             </span>
 
@@ -56,24 +57,26 @@ onMounted(() => {
         
         <ul class="top-[55px] md:flex md:items-center gap-10 z-[1] md:z-auto md:static absolute 
         w-full left-0 md:w-auto md:py-0 py-4 md:pl-0
-        transition-all ease-in-out duration-500 text-white h-screen md:h-full
-        *:py-6 *:md:py-0 *:text-[#242625] *:dark:text-white *:font-lato" :class="menuClass">
+        transition-all ease-in-out duration-500 text-white h-screen md:h-full bg-[#F7CAC9] dark:bg-[#242625] md:bg-transparent md:dark:bg-transparent
+        *:md:py-0 *:text-[#242625] *:dark:text-white *:text-2xl *:font-Alumni_Sans" :class="menuClass">
         
-            <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="/" class="text-xl hover:text-[#244e48] duration-500">Home</a>
-            </li>
-            <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="/projects" class="text-xl hover:text-[#244e48] duration-500">Projects</a>
-            </li>
-            <li class="hover:bg-[#9EC0BB] text-center">
-                <a href="#" class="text-xl hover:text-[#244e48] duration-500">About</a>
-            </li>
-            <li class="text-center">
-                <button class="bg-[#93a9d1] text-white font-bold duration-500 px-6 py-2 hover:bg-[#798499] rounded">
+            <RouterLink @click="toggleMenu()" to="/" class="hover:text-[#244e48] duration-500 text-center">
+                <li class="py-6 md:py-0 hover:bg-[#9EC0BB]">Home</li>
+            </RouterLink>
+            <RouterLink @click="toggleMenu()" to="/projects" class="hover:text-[#244e48] duration-500 text-center">
+                <li class="py-6 md:py-0 hover:bg-[#9EC0BB]">Projects</li>
+            </RouterLink>
+            <a @click="toggleMenu()" href="#about" class="hover:text-[#244e48] duration-500 text-center">
+                <li class="py-6 md:py-0 hover:bg-[#9EC0BB]">About</li>
+            </a>
+            <li class="text-center my-6 md:my-0">
+                <button @click="toggleMenu()" class="bg-[#93a9d1] text-white font-bold duration-500 px-6 py-2 hover:bg-[#798499] rounded">
                     Hire me
                 </button>
             </li>
-            <li class="text-center">
+            
+            <!-- </li> -->
+            <li class="mt-12 md:mt-0 hover:text-[#244e48] duration-500 text-center">
                 <DarkMode/>
             </li>
         </ul>
