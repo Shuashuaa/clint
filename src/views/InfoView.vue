289a1e82d49245d057a277ba23cd5199d60550ab
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-12 17:22:26 +0800

    121224

--- src/views/InfoView.vue
+++ src/views/InfoView.vue
@@ -6,8 +6,8 @@ defineProps({
 </script>
 
 <template>
-    <div class="flex justify-center w-full bg-white 
-    p-[15%] md:p-[5%] *:font-Inter *:text-lg ">
+    <div class="flex justify-center w-full bg-white selection:bg-[#F7CAC9] selection:text-[#495d81]
+    p-[15%] md:p-[5%] *:font-Inter *:text-lg *:tracking-wider">
     <!-- text-shadow shadow-[#bebebe] -->
         <p class="md:w-[50svw]">
             {{ info }}
