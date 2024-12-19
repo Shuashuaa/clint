ea48b05ef162f9a7a53829b4a1c6e27169ececb4
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-19 23:20:44 +0800

    121924

---
+++ README.md
@@ -0,0 +1,24 @@
+# clint
+
+
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+more information for infoView
+footer details
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+  
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+      fontFamily: {
+        "Alumni_Sans": ['Alumni Sans', 'sans-serif'],
+        "Alumni_Sans_Pinstripe": ['Alumni Sans Pinstripe', 'sans-serif'],
+        "Inter": ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
