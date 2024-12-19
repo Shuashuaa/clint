77c41d1c6ec7153c16a768e763dc2d1bdbccd9dc
Shuashuaa <jovijoshuatania027@gmail.com>
2024-12-19 22:35:56 +0800

    121924

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './assets/main.css'
+import './assets/style.css'
+import 'primeicons/primeicons.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// title meta
+router.beforeEach((to, from, next) => {
+    document.title = (to.meta.title as string) || '404';
+    next();
+});
+
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
