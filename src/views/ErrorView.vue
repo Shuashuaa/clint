3f388f6ecea8c68c0a1cdf1c0ba39e952f1bf42a
Shuashuaa <jovijoshuatania027@gmail.com>
2025-01-07 00:01:49 +0800

    010624

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-full text-center transition-all ease-in-out duration-1000">
+        <h1 class="h-[100vh] flex justify-center items-center hover:text-[40px] text-slate-700 dark:text-slate-200 overflow-hidden transition-all ease-in-out duration-1000">Oops, This page seems to be broken or not existing at all.</h1>
+    </div>
+</template>
